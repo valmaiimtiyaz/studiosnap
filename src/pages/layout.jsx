424ab7e37efed68d91f3096639939fa8ca8afbe8
valmaiimtiyaz <valmaityz30@gmail.com>
2025-12-10 19:23:55 +0700

    update

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import layout1 from "../assets/Layout1.png";
-import layout2 from "../assets/Layout2.png";
-import layout3 from "../assets/Layout3.png";
-import layout4 from "../assets/Layout4.png";
+import layout1 from "../assets/layout1.png";
+import layout2 from "../assets/layout2.png";
+import layout3 from "../assets/layout3.png";
+import layout4 from "../assets/layout4.png";
 
 const Layout = () => {
   const [selectedLayout, setSelectedLayout] = useState(null);
